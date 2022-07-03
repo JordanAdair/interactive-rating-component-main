0e9a9e308bb109fb3f4aadef310e6619b5ced8ad
Jordan <43575081+JordanAdair@users.noreply.github.com>
2022-07-03 15:14:19 -0400

    Added styling and basic html format

---
+++ interactive-rating-component-main/rating.js
@@ -0,0 +1,10 @@
+const ratingButton1 = document.getElementById("rating-1");
+const submitButton = document.getElementById("submit");
+
+ratingButton1.addEventListener("click", () => {
+	console.log("CLICK");
+});
+
+submitButton.addEventListener("click", () => {
+	console.log("CLICK");
+});
