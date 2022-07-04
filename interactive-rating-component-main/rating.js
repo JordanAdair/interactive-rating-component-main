6a80d5a564d935e921dd2aafaa3986c2f004c114
Jordan <43575081+JordanAdair@users.noreply.github.com>
2022-07-03 21:50:56 -0400

    Finished project

--- interactive-rating-component-main/rating.js
+++ interactive-rating-component-main/rating.js
@@ -1,10 +1,19 @@
-const ratingButton1 = document.getElementById("rating-1");
-const submitButton = document.getElementById("submit");
+const ratingWrapper = document.querySelector(".rating-wrapper");
+const thankYouWrapper = document.querySelector(".thank-you-wrapper");
+const selectedMessage = document.querySelector(".radio-val");
 
-ratingButton1.addEventListener("click", () => {
-	console.log("CLICK");
-});
+function initiateThankYouScreen() {
+	const ratings = document.getElementsByName("rating");
+	for (i = 0; i < ratings.length; i++) {
+		// Grab checked value
+		if (ratings[i].checked) {
+			console.log(ratings[i].value);
+			// Load the data inside of the empty selectedMessages <p> tag
+			selectedMessage.textContent = `You selected ${ratings[i].value} out of 5`;
 
-submitButton.addEventListener("click", () => {
-	console.log("CLICK");
-});
+			// Hide ratings, show thank you message.
+			ratingWrapper.classList.add("hidden");
+			thankYouWrapper.classList.remove("hidden");
+		}
+	}
+}
